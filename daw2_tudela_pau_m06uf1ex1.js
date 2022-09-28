c3e443e3c6ad8fb0c8d937ab075f9afc5b43bcb8
pautud22daw <113779199+pautud22daw@users.noreply.github.com>
2022-09-28 17:05:09 +0200

    Update daw2_tudela_pau_m06uf1ex1.js

--- daw2_tudela_pau_m06uf1ex1.js
+++ daw2_tudela_pau_m06uf1ex1.js
@@ -1,4 +1,4 @@
-let aC = [0,0];
+let aC = [0,0,0];
 let aT = ['','',''];
 let aR = [1,''];
 
@@ -12,8 +12,8 @@ window.onload = function() {
             case 1:
                 aT[1]='System selected Rock!';
                 if(aR[1] === 'rock'){
-                    aT[2]='You lose...';
-                    aC[0]++;
+                    aT[2]='Draw.';
+                    aC[2]++;
                 } else if(aR[1] === 'paper') {
                     aT[2]='You win!';
                     aC[1]++;
@@ -23,22 +23,24 @@ window.onload = function() {
                 } else{
                     aT[2]='You lose...';
                     aC[0]++;
-                } break;
+                } 
+                break;
             case 2:
                 aT[1]='System selected Paper!';
                 if(aR[1] === 'rock'){
                     aT[2]='You lose...';
                     aC[0]++;
                 } else if(aR[1] === 'paper') {
-                    aT[2]='You lose...';
-                    aC[0]++;
+                    aT[2]='Draw.';
+                    aC[2]++;
                 } else if(aR[1] === 'scissors'){
                     aT[2]='You win!';
                     aC[1]++;
                 } else{
                     aT[2]='You lose...';
                     aC[0]++;
-                } break;
+                } 
+                break;
             case 3:
                 aT[1]='System selected Scissor!';
                 if(aR[1] === 'rock'){
@@ -48,14 +50,14 @@ window.onload = function() {
                     aT[2]='You lose...';
                     aC[0]++;
                 } else if(aR[1] === 'scissors'){
-                    aT[2]='You lose...';
-                    aC[0]++;
+                    aT[2]='Draw.';
+                    aC[2]++;
                 }else{
                     aT[2]='You lose...';
                     aC[0]++;
                 } 
         }
-        document.getElementById('exit1').innerHTML=`<b>User: ${aC[1]} | System: ${aC[0]}</b>`;
+        document.getElementById('exit1').innerHTML=`<b>User: ${aC[1]} | System: ${aC[0]} | Draws: ${aC[2]}</b>`;
         document.getElementById('exit2').innerHTML=`<b>${aT[1]}</b>`;
         setTimeout(500);
         document.getElementById('exit3').innerHTML=`<b>${aT[2]}</b>`;
